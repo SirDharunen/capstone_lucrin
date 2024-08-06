12094967a7b68e969518751626ce1d3b32734403
Dharunen <dharunen.periacarpen@student.curtin.edu.au>
2024-08-07 00:05:37 +0400

    2 index files
    Implement controllers for both admin and frontend. These files handle the logic for processing requests and preparing responses for the module's admin and frontend pages.

---
+++ app/code/Mastering/SampleModule/Controller/Index/Index.php
@@ -0,0 +1,40 @@
+<?php
+namespace Mastering\SampleModule\Controller\Index;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\Controller\ResultInterface;
+
+class Index implements HttpGetActionInterface
+{
+    /**
+     * @var PageFactory
+     */
+    private PageFactory $resultPageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Execute action based on request and return result
+     *
+     * Note: Request will be added as operation argument in future
+     *
+     * @return ResultInterface
+     */
+    public function execute(): ResultInterface
+    {
+        return $this->resultPageFactory->create();
+    }
+}
