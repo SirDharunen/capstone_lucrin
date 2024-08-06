5fe01ae81614e8f26953781d4703b9afa373240f
Dharunen <dharunen.periacarpen@student.curtin.edu.au>
2024-08-07 00:07:52 +0400

    Implement frontend block and templates for SampleModule
    Add frontend rendering logic and templates. These files define how data is prepared for display (Block) and how it's presented to the user (layout and template), completing the frontend implementation of the module.

---
+++ app/code/Mastering/SampleModule/Block/Hello.php
@@ -0,0 +1,15 @@
+<?php
+namespace Mastering\SampleModule\Block;
+
+class Hello extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+    {
+        parent::__construct($context);
+    }
+
+    public function sayHello()
+    {
+        return __('Hello World');
+    }
+}
